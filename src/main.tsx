59976a8bb1d04d46e2cfc62db58cec6ade41925f
unknown <pdemeu@outlook.com>
2023-01-31 01:24:59 -0300

    resetando css default

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
