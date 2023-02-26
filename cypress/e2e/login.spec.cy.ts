97a19cbe5448733c99b2b18aeda6fba9c3b76221
Pedro Demeu <pdemeu@segware.com>
2023-02-26 15:28:19 -0300

    test: Primeiro teste CYPRESS do projeto

--- cypress/e2e/login.spec.cy.ts
+++ cypress/e2e/login.spec.cy.ts
@@ -2,12 +2,12 @@
 
 describe('Login', () => {
   it('login successfully', () => {
-    cy.visit('http://localhost:5173/')
-    cy.wait(2000)
-    cy.get('input[name="username"]').type('user123')
-    cy.get('input[name="password"]').type('1234')
-    cy.get('button[type="submit"]').click()
-    cy.url().should('include', '/home')
-  })
-})
+    cy.visit('http://localhost:5173/');
+    cy.wait(2000);
+    cy.get('input[name="username"]').type('user123');
+    cy.get('input[name="password"]').type('1234');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/home');
+  });
+});
  
