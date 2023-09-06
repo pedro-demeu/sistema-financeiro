5bb4f1a60c8968f373962f9c0b52af02bd2f544d
pdemeu <pdemeu@outlook.com>
2023-09-06 02:42:31 -0300

    feat: created my first route to handle categories

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import { config } from 'dotenv';
+import { routes as CategoryRoutes } from './modules/category';
+config();
 
 const app = express();
 
 app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
+
+app.use('/v1/api/category', CategoryRoutes);
