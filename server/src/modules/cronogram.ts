a932c170a5cb67036c0d2eee3cffccc642133096
pdemeu <pdemeu@outlook.com>
2023-09-11 04:31:47 -0300

    feat: created all controllers to cronogram

---
+++ server/src/modules/cronogram.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+export const routes = Router();
+routes.get('/', async (req, res) => {});
+routes.post('/', async (req, res) => {});
+routes.get('/:id', async (req, res) => {});
+routes.put('/:id', async (req, res) => {});
+routes.delete('/:id', async (req, res) => {});
